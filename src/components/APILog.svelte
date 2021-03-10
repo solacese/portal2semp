0bd2b2e9159036d43607b54b45bbf0926b479692
TomFairbairnSolace <tom.fairbairn@solace.com>
2021-03-10 18:06:47 +0000

    Add APILog component to Git.  Oops.

---
+++ src/components/APILog.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let title = "API logs";
+  export let logString = "";
+
+  export const apiGet = (description, url, options) => {
+    logString += description + " -> GET: " + url + "\n";
+    return fetch(url, options);
+  }
+
+  const clearLog = () => {
+    logString = "";
+  }
+</script>
+
+<p>
+<div style="width:100%;display:block">
+<hr>
+<h2>Logs: what was sent to the {title}</h2>
+<textarea rows="6" cols="80" readonly="true" placeholder={title}>
+{logString}
+</textarea>
+<button on:click={clearLog}>Clear log</button>
+</div>
