f3ec2e6dfe9910dd7476f8e5f37df52bfcb31f31
Tom Fairbairn <tom.fairbairn@solace.com>
2022-05-27 12:14:38 +0100

    Update EP Rest API endpoint location

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export let config = {
   token: "Insert your API token",
-  portalUrl: "https://solace.cloud/api/v1/eventPortal/",
+  portalUrl: "https://api.solace.cloud/api/v1/eventPortal/",
   applicationDomainSuffix: "applicationDomains",
   applicationDomainName: "MT-demo",
   brokerUrl: "http://localhost:8080",
